aa76a04f6c5cf79ae7b41fdbfb0743539167cc28
Max Hoffmann <mho@nanologika.de>
2021-02-18 18:40:52 +0100

    added 'all' category filter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
-const CLASS_SELECTED = 'selected';
-const ID_WORDCLOUD = 'wordcloud';
 
 window.onload = function(){
     intiateDraggable();
     intitiateWordcloudFilter();
 }
+
+
+/**
+ * Drag & Drop
+ */
 
 function intiateDraggable(){
     const Classes = {
@@ -19,6 +22,16 @@ function intiateDraggable(){
     });
 }
 
+
+
+/**
+ * Wordcloud Filter
+ */
+
+const CLASS_SELECTED = 'selected';
+const ID_WORDCLOUD = 'wordcloud';
+const ID_SHOW_ALL_CATEGORIES = 'show-all-categories';
+
 function intitiateWordcloudFilter(){
     const wordcloud = document.getElementById(ID_WORDCLOUD).getElementsByTagName('li');
     for(li of wordcloud){
@@ -28,6 +41,19 @@ function intitiateWordcloudFilter(){
 }
 
 function updateWordcloudFilter(){
+    if(this.id === ID_SHOW_ALL_CATEGORIES){
+        if(!this.className.includes(CLASS_SELECTED)){
+            this.className = this.className.concat(CLASS_SELECTED);
+            showAllModules();
+            return;
+        } else {
+            const thisClasses = this.className.split(' ');
+            const thisClassesWithoutSelected = thisClasses.filter(className => className != CLASS_SELECTED);
+            this.className = thisClassesWithoutSelected.join(' ');
+            hideAllModules();
+            return;
+        }
+    }
     if(!this.className.includes(CLASS_SELECTED)){
         this.className = this.className.concat(CLASS_SELECTED);
     } else {
@@ -38,6 +64,33 @@ function updateWordcloudFilter(){
     updateSelectableModulesList();
 }
 
+function hideAllCategories(){
+    const wordcloud = Array.from(document.getElementById(ID_WORDCLOUD).getElementsByTagName('li'));
+    for(category of wordcloud){
+        if(category.id != ID_SHOW_ALL_CATEGORIES){
+            const categoryClasses = category.className.split(' ');
+            const categoryClassesWithoutSelected = categoryClasses.filter(className => className != CLASS_SELECTED);
+            category.className = categoryClassesWithoutSelected.join(' ');
+        }
+    }
+}
+
+function showAllModules(){
+    hideAllCategories()
+    const sideBarModules = Array.from(document.getElementById('side-bar-module-list').getElementsByClassName('module'));
+    for(mod of sideBarModules){
+        mod.style.display = '';
+    }
+}
+
+function hideAllModules(){
+    hideAllCategories()
+    const sideBarModules = Array.from(document.getElementById('side-bar-module-list').getElementsByClassName('module'));
+    for(mod of sideBarModules){
+        mod.style.display = 'none';
+    }
+}
+
 function updateSelectableModulesList(){
     const wordcloud = Array.from(document.getElementById(ID_WORDCLOUD).getElementsByTagName('li'));
     const wordcloudSelectedCategories = wordcloud.filter(li => li.className.includes(CLASS_SELECTED));
@@ -45,10 +98,14 @@ function updateSelectableModulesList(){
 
     const sideBarModules = Array.from(document.getElementById('side-bar-module-list').getElementsByClassName('module'));
 
+    let showAllModulesCategory = document.getElementById(ID_SHOW_ALL_CATEGORIES)
+    const showAllModulesCategoryClasses = showAllModulesCategory.className.split(' ');
+    const showAllModulesCategoryClassesWithoutSelected = showAllModulesCategoryClasses.filter(className => className != CLASS_SELECTED);
+    showAllModulesCategory.className = showAllModulesCategoryClassesWithoutSelected.join(' ');
+
     if(selectedCategories.length == 0){
-        for(mod of sideBarModules){
-            mod.style.display = 'none';
-        }
+        hideAllModules();
+        return;
     }
 
     for(mod of sideBarModules){
