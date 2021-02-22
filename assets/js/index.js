62c752e7a61f8250e666e7f647e518736fd05571
Max Hoffmann <mho@nanologika.de>
2021-02-22 18:15:40 +0100

    implemented sortableJS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+const MODULE_LIST_SIDE_BAR = 'module-list-side-bar';
 
 window.onload = function(){
     intiateDraggable();
@@ -10,15 +11,45 @@ window.onload = function(){
  */
 
 function intiateDraggable(){
-    const Classes = {
-        draggable: 'StackedListItem--isDraggable',
-        capacity: 'draggable-container-parent--capacity',
-    };
+    let index = 0;
+    let resource_list_lists = document.getElementsByClassName('resource-list');
+    for(ul of resource_list_lists){
+        Sortable.create(ul, {
+            group: {
+                name: `resource-list-${index}`,
+                put: function(){
+                    const chosenClassName = document.getElementsByClassName('sortable-chosen')[0].className;
+                    const isResource = chosenClassName.includes('resource');
+                    const isTimeBreak = chosenClassName.includes('timebreak');
+                    return isResource || isTimeBreak;
+                }
+            },
+            fallbackOnBody: true,
+            animation: 100
+        });
+        index++;
+    }
 
-    const containers = document.querySelectorAll('#MultipleContainers .StackedList');
+    let module_list_training = document.getElementById('module-list-training');
+    Sortable.create(module_list_training, {
+        group: {
+            name: 'module_list_training',
+            put: function(){
+                const chosenClassName = document.getElementsByClassName('sortable-chosen')[0].className;
+                const isModule = chosenClassName.includes('module');
+                const isTimeBreak = chosenClassName.includes('timebreak');
+                return isModule || isTimeBreak;
+            }
+        },
+        fallbackOnBody: true,
+		swapThreshold: 0.2,
+        animation: 100
+    });
 
-    const sortable = new Draggable.Sortable(containers, {
-        draggable: `.${Classes.draggable}`
+    let module_list_side_bar = document.getElementById(MODULE_LIST_SIDE_BAR);
+    Sortable.create(module_list_side_bar, {
+        group: MODULE_LIST_SIDE_BAR,
+        animation: 100
     });
 }
 
@@ -30,7 +61,7 @@ function intiateDraggable(){
 
 const CLASS_SELECTED = 'selected';
 const ID_WORDCLOUD = 'wordcloud';
-const ID_SHOW_ALL_CATEGORIES = 'show-all-categories';
+const ID_SHOW_ALL_CATEGORIES = 'show-all-modules';
 
 function intitiateWordcloudFilter(){
     const wordcloud = document.getElementById(ID_WORDCLOUD).getElementsByTagName('li');
@@ -64,7 +95,7 @@ function updateWordcloudFilter(){
     updateSelectableModulesList();
 }
 
-function hideAllCategories(){
+function unselectAllCategories(){
     const wordcloud = Array.from(document.getElementById(ID_WORDCLOUD).getElementsByTagName('li'));
     for(category of wordcloud){
         if(category.id != ID_SHOW_ALL_CATEGORIES){
@@ -76,16 +107,16 @@ function hideAllCategories(){
 }
 
 function showAllModules(){
-    hideAllCategories()
-    const sideBarModules = Array.from(document.getElementById('side-bar-module-list').getElementsByClassName('module'));
+    unselectAllCategories()
+    const sideBarModules = Array.from(document.getElementById(MODULE_LIST_SIDE_BAR).getElementsByClassName('module'));
     for(mod of sideBarModules){
         mod.style.display = '';
     }
 }
 
 function hideAllModules(){
-    hideAllCategories()
-    const sideBarModules = Array.from(document.getElementById('side-bar-module-list').getElementsByClassName('module'));
+    unselectAllCategories()
+    const sideBarModules = Array.from(document.getElementById(MODULE_LIST_SIDE_BAR).getElementsByClassName('module'));
     for(mod of sideBarModules){
         mod.style.display = 'none';
     }
@@ -96,7 +127,7 @@ function updateSelectableModulesList(){
     const wordcloudSelectedCategories = wordcloud.filter(li => li.className.includes(CLASS_SELECTED));
     const selectedCategories = wordcloudSelectedCategories.map(li => li.textContent);
 
-    const sideBarModules = Array.from(document.getElementById('side-bar-module-list').getElementsByClassName('module'));
+    const sideBarModules = Array.from(document.getElementById(MODULE_LIST_SIDE_BAR).getElementsByClassName('module'));
 
     let showAllModulesCategory = document.getElementById(ID_SHOW_ALL_CATEGORIES)
     const showAllModulesCategoryClasses = showAllModulesCategory.className.split(' ');
