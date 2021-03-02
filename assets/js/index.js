7c866007a54466dfe6f1cf0e1cb2897a9f3337f5
Max Hoffmann <maximilian.hoffmann@nanologika.de>
2021-03-02 19:01:37 +0100

    implemented helper functions for time calculation

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ const ID_MODULE_LIST_TRAINING = 'module-list-training';
  * Drag & Drop
  */
 
-function initiateSortable(){
+function initiateSortable() {
     let moduleListTraining = document.getElementById(ID_MODULE_LIST_TRAINING);
     Sortable.create(moduleListTraining, {
         group: {
@@ -14,7 +14,7 @@ function initiateSortable(){
             put: true
         },
         fallbackOnBody: true,
-		swapThreshold: 0.2,
+        swapThreshold: 0.2,
         animation: 100,
         onAdd: runDynamicCalculationsOnAdd,
         onUpdate: runDynamicCalculationsOnUpdate
@@ -37,18 +37,18 @@ function initiateSortable(){
 
     let index = 0;
     let resourceListLists = document.getElementsByClassName('resource-list');
-    for(ul of resourceListLists){
+    for (ul of resourceListLists) {
         Sortable.create(ul, {
             group: {
                 name: `resource-list-${index}`,
-                put: function(){
+                put: function () {
                     const chosenClassName = document.getElementsByClassName('sortable-chosen')[0].className;
                     const isResource = chosenClassName.includes('resource');
                     const isTimeBreak = chosenClassName.includes('timebreak');
                     const isDayBreak = chosenClassName.includes('daybreak');
                     return isResource || isTimeBreak || isDayBreak;
                 },
-                pull: function(){
+                pull: function () {
                     const chosenClassName = document.getElementsByClassName('sortable-chosen')[0].className;
                     const isTimeBreak = chosenClassName.includes('timebreak');
                     const isDayBreak = chosenClassName.includes('daybreak');
@@ -66,19 +66,19 @@ function initiateSortable(){
     initiateTrashButton();
 }
 
-function initiateTrashButton(){
+function initiateTrashButton() {
     let trashButtons = document.getElementsByClassName('trash');
-    for(btn of trashButtons){
+    for (btn of trashButtons) {
         btn.onclick = onClickDeleteOrMoveListElement
     }
 }
 
-function onClickDeleteOrMoveListElement(){    
+function onClickDeleteOrMoveListElement() {
     let currentElement = this;
-    while(currentElement.tagName !== 'LI'){
+    while (currentElement.tagName !== 'LI') {
         currentElement = currentElement.parentNode;
     }
-    if(currentElement.className.includes('module')){
+    if (currentElement.className.includes('module')) {
         let moduleListSideBar = document.getElementById(ID_MODULE_LIST_SIDE_BAR);
         moduleListSideBar.appendChild(currentElement);
         return;
@@ -90,32 +90,91 @@ function onClickDeleteOrMoveListElement(){
  * Dynamic Calculations
  */
 
-function runDynamicCalculationsOnUpdate(){
+function runDynamicCalculationsOnUpdate() {
     calculateTime();
     calculateSummary();
 }
 
-function runDynamicCalculationsOnAdd(evt){
+function runDynamicCalculationsOnAdd(evt) {
     let mod = evt.item;
     insertTimeBreaks(mod);
     calculateTime();
     calculateSummary();
 }
 
-function calculateTime(){
+function calculateTime() {
     let moduleList = Array.from(document.getElementById(ID_MODULE_LIST_TRAINING).childNodes);
     moduleList = moduleList.filter(el => el.nodeName.includes('LI'));
 
     let totalTime = 0;
-    let moduleTime = 0;
-    for(mod of moduleList){
-        if(mod.className.includes('module')){
+    let clockTime = new Date();
+    for (mod of moduleList) {
+        if (mod.className.includes('module')) {
             let resources = document.querySelectorAll(`#${mod.id} .resource`);
+            // TODO
+        } else if (mod.className.includes('timebreak')) {
+            let duration = parseInt(mod.dataset.duration);
+            // TODO
+        } else if(mod.className.includes('daybreak')){
+            let duration = parseInt(mod.dataset.duration);
+            // TODO
+        } else if (mod.className.includes('trainingstart')) {
+            const duration = parseInt(mod.dataset.duration);
+            clockTime = parseDatefromString(clockTime, mod.dataset.start);
+            clockTime = insertClockTime(clockTime, duration, mod);
+            totalTime+=duration;
         }
     }
 }
 
-function calculateSummary(){
+function parseDatefromString(clockTime, daytime) {
+    const splitDaytime = daytime.split(':');
+    clockTime.setHours(splitDaytime[0]);
+    clockTime.setMinutes(splitDaytime[1]);
+    return clockTime;
+}
+
+function insertClockTime(clockTime, duration, mod) {
+    const oldClockTime = new Date(clockTime);
+    clockTime = new Date(clockTime.getTime() + duration * 60 * 1000);
+    const clockTimeString = `${convertTimeToString(oldClockTime)} - ${convertTimeToString(clockTime)}`;
+    let clockTimeSpan = getChildByClassName(mod, 'clock-time');
+    clockTimeSpan.innerText = clockTimeString;
+}
+
+function convertTimeToString(clockTime) {
+    let hour = clockTime.getHours();
+    let minute = clockTime.getMinutes();
+    let second = clockTime.getSeconds();
+    let temp = '' + ((hour > 12) ? hour - 12 : hour);
+    if (hour == 0)
+        temp = '12';
+    temp += ((minute < 10) ? ':0' : ':') + minute;
+    temp += (hour >= 12) ? 'pm' : 'am';
+    return temp;
+}
+
+// this function returns the first child node matching the className it finds at the nearest point to the root
+function getChildByClassName(el, className){
+    let runLoop = true;
+    let nextLevelChilds = el.childNodes;
+    while(runLoop){
+        let currentChilds = nextLevelChilds;
+        nextLevelChilds = [];
+        for(let i = 0; i < currentChilds.length; i++) {
+            nextLevelChilds.push(...currentChilds[i].childNodes);
+            if (currentChilds[i].className == className) {
+                return currentChilds[i];
+            }
+        }
+        if(nextLevelChilds.length == 0){
+            return null;
+        }
+        currentChilds = nextLevelChilds;
+    }
+}
+
+function calculateSummary() {
     // TODO
 }
 
@@ -125,52 +184,52 @@ function calculateSummary(){
 
 const BREAK_INTERVAL = 90;
 
-function initiateTimeBreaks(){
+function initiateTimeBreaks() {
     let moduleListTraining = Array.from(document.getElementById(ID_MODULE_LIST_TRAINING).childNodes);
     moduleListTraining = moduleListTraining.filter(el => el.nodeName.includes('LI') && el.className.includes('module'));
-    for(mod of moduleListTraining){
+    for (mod of moduleListTraining) {
         insertTimeBreaks(mod);
     }
 }
 
-function insertTimeBreaks(mod){
+function insertTimeBreaks(mod) {
     let moduleList = Array.from(document.getElementById(ID_MODULE_LIST_TRAINING).childNodes);
     moduleList = moduleList.filter(el => el.nodeName.includes('LI') && el.className.includes('module'));
     const isLastModule = moduleList[moduleList.length - 1] === mod;
 
     let durationSum = 0;
     let resources = document.querySelectorAll(`#${mod.id} .resource`);
-    for(resource of resources){
+    for (resource of resources) {
         const duration = parseInt(resource.dataset.duration);
         const isLastResource = resources[resources.length - 1] === resource;
         let hasBreakAfter = false;
         let searchBreak = true;
         let currentElement = resource;
-        while(searchBreak){ // we do it this way of some strange html (nodeName: '#text') siblings appear on the rendered side inbetween the list elems
+        while (searchBreak) { // we do it this way of some strange html (nodeName: '#text') siblings appear on the rendered side inbetween the list elems
             currentElement = currentElement.nextSibling;
-            if(currentElement != null && currentElement.nodeName === 'LI'&& (currentElement.className.includes('timebreak') || currentElement.className.includes('resource'))){
-                if(currentElement.className.includes('timebreak')){
+            if (currentElement != null && currentElement.nodeName === 'LI' && (currentElement.className.includes('timebreak') || currentElement.className.includes('resource'))) {
+                if (currentElement.className.includes('timebreak')) {
                     hasBreakAfter = true;
                     searchBreak = false;
-                } 
+                }
                 searchBreak = false;
             }
-            if(currentElement === null){
+            if (currentElement === null) {
                 searchBreak = false;
             }
         }
 
-        if(!isNaN(duration)){
+        if (!isNaN(duration)) {
             durationSum += duration;
         }
-        if(duration >= BREAK_INTERVAL && !(isLastModule && isLastResource) && !hasBreakAfter){
+        if (duration >= BREAK_INTERVAL && !(isLastModule && isLastResource) && !hasBreakAfter) {
             addTimeBreakAfter(resource);
             durationSum = 0;
         }
     }
 }
 
-function addTimeBreakAfter(resource){
+function addTimeBreakAfter(resource) {
     const MODULE_TIME_BREAK = document.getElementsByClassName('timebreak')[0].cloneNode(true);
     resource.parentNode.insertBefore(MODULE_TIME_BREAK, resource.nextSibling);
     initiateTrashButton();
@@ -184,17 +243,17 @@ const CLASS_SELECTED = 'selected';
 const ID_WORDCLOUD = 'wordcloud';
 const ID_SHOW_ALL_CATEGORIES = 'show-all-modules';
 
-function initiateWordcloudFilter(){
+function initiateWordcloudFilter() {
     const wordcloud = document.getElementById(ID_WORDCLOUD).getElementsByTagName('li');
-    for(li of wordcloud){
+    for (li of wordcloud) {
         li.onclick = updateWordcloudFilter;
     }
     updateSelectableModulesList();
 }
 
-function updateWordcloudFilter(){
-    if(this.id === ID_SHOW_ALL_CATEGORIES){
-        if(!this.className.includes(CLASS_SELECTED)){
+function updateWordcloudFilter() {
+    if (this.id === ID_SHOW_ALL_CATEGORIES) {
+        if (!this.className.includes(CLASS_SELECTED)) {
             this.className = this.className.concat(CLASS_SELECTED);
             showAllModules();
             return;
@@ -206,7 +265,7 @@ function updateWordcloudFilter(){
             return;
         }
     }
-    if(!this.className.includes(CLASS_SELECTED)){
+    if (!this.className.includes(CLASS_SELECTED)) {
         this.className = this.className.concat(CLASS_SELECTED);
     } else {
         const thisClasses = this.className.split(' ');
@@ -216,10 +275,10 @@ function updateWordcloudFilter(){
     updateSelectableModulesList();
 }
 
-function unselectAllCategories(){
+function unselectAllCategories() {
     const wordcloud = Array.from(document.getElementById(ID_WORDCLOUD).getElementsByTagName('li'));
-    for(category of wordcloud){
-        if(category.id != ID_SHOW_ALL_CATEGORIES){
+    for (category of wordcloud) {
+        if (category.id != ID_SHOW_ALL_CATEGORIES) {
             const categoryClasses = category.className.split(' ');
             const categoryClassesWithoutSelected = categoryClasses.filter(className => className != CLASS_SELECTED);
             category.className = categoryClassesWithoutSelected.join(' ');
@@ -227,23 +286,23 @@ function unselectAllCategories(){
     }
 }
 
-function showAllModules(){
+function showAllModules() {
     unselectAllCategories()
     const sideBarModules = Array.from(document.getElementById(ID_MODULE_LIST_SIDE_BAR).getElementsByClassName('module'));
-    for(mod of sideBarModules){
+    for (mod of sideBarModules) {
         mod.style.display = '';
     }
 }
 
-function hideAllModules(){
+function hideAllModules() {
     unselectAllCategories()
     const sideBarModules = Array.from(document.getElementById(ID_MODULE_LIST_SIDE_BAR).getElementsByClassName('module'));
-    for(mod of sideBarModules){
+    for (mod of sideBarModules) {
         mod.style.display = 'none';
     }
 }
 
-function updateSelectableModulesList(){
+function updateSelectableModulesList() {
     const wordcloud = Array.from(document.getElementById(ID_WORDCLOUD).getElementsByTagName('li'));
     const wordcloudSelectedCategories = wordcloud.filter(li => li.className.includes(CLASS_SELECTED));
     const selectedCategories = wordcloudSelectedCategories.map(li => li.textContent);
@@ -255,15 +314,15 @@ function updateSelectableModulesList(){
     const showAllModulesCategoryClassesWithoutSelected = showAllModulesCategoryClasses.filter(className => className != CLASS_SELECTED);
     showAllModulesCategory.className = showAllModulesCategoryClassesWithoutSelected.join(' ');
 
-    if(selectedCategories.length == 0){
+    if (selectedCategories.length == 0) {
         hideAllModules();
         return;
     }
 
-    for(mod of sideBarModules){
+    for (mod of sideBarModules) {
         const modClasses = mod.className.split(' ');
-        for(category of selectedCategories){
-            if(modClasses.includes(category)){
+        for (category of selectedCategories) {
+            if (modClasses.includes(category)) {
                 mod.style.display = '';
                 break; // --> change around here to implement "AND" filter style. bahaves atm like "OR"
             } else {
@@ -276,7 +335,7 @@ function updateSelectableModulesList(){
 /**
  * and here we go
  */
-window.onload = function(){
+window.onload = function () {
     initiateSortable();
     initiateWordcloudFilter();
     initiateTimeBreaks();
